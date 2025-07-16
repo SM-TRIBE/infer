7c47a76a3168170c43bd76886a7d4309171f18ae
Nima <110401327+SM-TRIBE@users.noreply.github.com>
2025-07-16 15:39:42 +0330

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+# utils.py
+# This file contains utility functions that can be used across the application.
+# For now, it's a placeholder for any future helper functions you might need.
+
+def format_user_for_display(user_data):
+    """
+    A sample utility function to format user data.
+    (Not currently used, but shows the purpose of this file)
+    """
+    return f"User: {user_data['name']} (ID: {user_data['user_id']})"
